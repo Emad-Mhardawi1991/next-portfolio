7a4ffa29c4aefd42cab06c1b3d175a0dfaef46bc
emad.mhardawi1991 <emad.mhardawi@protonmail.com>
2022-07-16 12:23:33 +0200

    add animation on scroll to about section

--- components/ui/aboutCard/aboutCard.js
+++ components/ui/aboutCard/aboutCard.js
@@ -1,16 +1,20 @@
 import classes from "./aboutCard.module.scss";
+import {motion} from "framer-motion";
 
 const AboutCard = (props) => {
   return (
-    <div className={classes.aboutCard}>
+    <motion.div
+      className={classes.aboutCard}
+      whileInView={{ opacity: [0, 1], transition:{duration: 1} }}
+      whileHover={{ scale: 1.1 }}
+      transition={{ duration: 0.5, type: "tween" }}
+      key={props.about.title + props.about.index}
+    >
       <img src={props.about.imageUrl} alt="card" />
       <h3>{props.about.title}</h3>
       <p className={classes.description}> {props.about.description}</p>
-    </div>
+    </motion.div>
   );
 };
 
 export default AboutCard;
-
-
-
