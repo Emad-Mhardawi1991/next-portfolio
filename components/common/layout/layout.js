d1800c5571b381faeffaaaa6fece72553ef716de
emad.mhardawi1991 <emad.mhardawi@protonmail.com>
2022-07-16 09:41:00 +0200

    add layout component

---
+++ components/common/layout/layout.js
@@ -0,0 +1,16 @@
+const Layout = (props) => {
+  return (
+    <div>
+      <header>
+        
+      </header>
+      {props.children}
+
+      <footer>
+         
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
